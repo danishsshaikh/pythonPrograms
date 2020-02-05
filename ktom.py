c75f8596ce9805fb23e5b3511c8185d93c38213f
danishsshaikh <danishsshaikh@gmail.com>
2020-02-05 20:22:02 +0530

    kilometers to miles

---
+++ ktom.py
@@ -0,0 +1,7 @@
+kilometers = float(input('Enter value in Kilometeres: '))
+
+conFactor = 0.621371
+
+miles = kilometers * conFactor
+
+print('{0} Kilometers is Equal to {1}'.format(kilometers,miles))
