70ed408c76efd659e4c6fe86b1a2ed4308ebca81
danishsshaikh <danishsshaikh@gmail.com>
2020-03-04 23:14:09 +0530

    Number of Vowels in a String

---
+++ vowelStr.py
@@ -0,0 +1,11 @@
+string = input("Enter String:")
+
+vowels = 0
+
+for i in string:
+    if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+            vowels=vowels+1
+
+print('Number of Vowels are:')
+
+print(vowels)
