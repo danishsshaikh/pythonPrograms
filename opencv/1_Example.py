e09fd2fe9763c62834bb516158805bac258bbaa2
danishsshaikh <danishsshaikh@gmail.com>
2020-03-05 23:14:12 +0530

    Array slicing

--- opencv/1_Example.py
+++ opencv/1_Example.py
@@ -2,6 +2,10 @@ import imutils
 import cv2
 
 image = cv2.imread("jp.png")
-print(image.shape)
+#print(image.shape)
 cv2.imshow("Jurassic Park",image)
+#cv2.waitKey(0)
+
+roi = image[60:160, 320:420]
+cv2.imshow("ROI", roi)
 cv2.waitKey(0)
