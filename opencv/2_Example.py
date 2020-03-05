b748c7a738a43f3f1f982c9af1ff9282aa74547f
danishsshaikh <danishsshaikh@gmail.com>
2020-03-06 00:34:11 +0530

    Line & Text

--- opencv/2_Example.py
+++ opencv/2_Example.py
@@ -14,7 +14,16 @@ cv2.waitKey(0)
 
 #Circle
 cv2.circle(output,(300, 150), 20, (255, 0, 0), -1)
-cv2.imshow("Circle", output)
+#cv2.imshow("Circle", output)
 cv2.waitKey(0)
 
+#Line
+cv2.line(output, (60, 20), (400, 200), (0, 0, 255), 5)
+#cv2.imshow("Line", output)
+cv2.waitKey(0)
+
+cv2.putText(output, "DAN", (10, 25), 
+	cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+cv2.imshow("Text", output)
+cv2.waitKey(0)
 
