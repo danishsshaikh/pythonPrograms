351c242ef61006d43e80f0b660c147551c418cf3
danishsshaikh <danishsshaikh@gmail.com>
2020-03-05 23:41:37 +0530

    Circle

--- opencv/2_Example.py
+++ opencv/2_Example.py
@@ -7,6 +7,14 @@ image = cv2.imread("jp.png")
 #cv2.waitKey(0)
 
 output = image.copy()
-cv2.rectangle(output, (320,60), (420, 160), (0, 0, 255), 2)
-cv2.imshow("Rectangle",output)
+#Rectangle
+cv2.rectangle(output, (320,60), (420, 160), (0, 255, 255), 5)
+#cv2.imshow("Rectangle",output)
 cv2.waitKey(0)
+
+#Circle
+cv2.circle(output,(300, 150), 20, (255, 0, 0), -1)
+cv2.imshow("Circle", output)
+cv2.waitKey(0)
+
+
